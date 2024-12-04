33f1ef84a0fbabdffb4566481b7614bbb2cb4282
jonid <krassjonid@gmail.com>
2024-12-04 16:50:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// JavaScript to handle the "Add to Cart" functionality (for demonstration purposes)
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
+
+
+
+
